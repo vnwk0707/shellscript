3029b1c760f8f8a2a66999803564d89e4f310080
venkat <vnwk05@gmail.com>
2025-01-02 19:24:13 +0530

    conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rahim:: Hi Robert"
+echo "Robert:: Hello Rahim"
+echo "Rahim:: How are you doing?"
+echo "Robert:: I am good. How are you?"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh # no space before and after equal
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "${PERSON2}:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am good. How are you?"
